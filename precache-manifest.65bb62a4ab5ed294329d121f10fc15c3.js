af6d9ef1efd830a1f5c9ed73e2651e559e92eb55
Anh Nguyen <a.nguyen0208@gmail.com>
2020-09-30 19:02:17 -0500

    Updates

---
+++ precache-manifest.65bb62a4ab5ed294329d121f10fc15c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb0e1a00bb080193130c5d5075a9bbe",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "8d024b0f90fc0607c404",
+    "url": "/random-quote-machine/static/css/main.9942ef97.chunk.css"
+  },
+  {
+    "revision": "d2f82a250ca858e8ca53",
+    "url": "/random-quote-machine/static/js/2.0f252d38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.0f252d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d024b0f90fc0607c404",
+    "url": "/random-quote-machine/static/js/main.5f8a0267.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
